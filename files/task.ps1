bc3ebb61794f0ed0f07fd337c79c280f84a0d693
Nikolay Kolev <nkolev@unsinc.com>
2024-01-16 01:30:53 +0200

    Scheduled task

--- files/task.ps1
+++ files/task.ps1
@@ -1,23 +1,16 @@
 # Define the repository
 $user = "unsinc"
 $repo = "siemagent"
-$token = "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"
 $tempPath = "C:\Windows\Temp\UnsAgentUpdater.log"
-
-# Create a header with your token
-$headers = @{
-    "Authorization" = "token $token"
-}
 
 try {
     # Call the GitHub API
-    $response = Invoke-RestMethod -Uri "https://api.github.com/repos/$user/$repo/releases/latest" -Headers $headers
+    $response = Invoke-RestMethod -Uri "https://api.github.com/repos/$user/$repo/releases/latest" -UseBasicParsing
 }
 catch {
     $errorMessage = $_.Exception
     Write-Output "Unable to get response from the server for following reasons: $errorMessage" | OutFi;e -FilePath $tempPath -Append -ErrorAction SilentlyContinue
 }
-
 # Check the tag_name property for the latest release version
 $latestVersion = $response.tag_name
 
@@ -31,7 +24,7 @@ if ($latestVersion -eq "2024.01.15") {
     try {
         # Download the script
         $scriptPath = Join-Path $env:TEMP "update.ps1"
-        Invoke-RestMethod -Uri "https://raw.githubusercontent.com/unsinc/siemagent/testing/files/update.ps1" -Headers $headers -OutFile $scriptPath
+        Invoke-RestMethod -Uri "https://raw.githubusercontent.com/$user/$repo/files/update.ps1" -UseBasicParsing -OutFile $scriptPath
 
         # Execute the script
         & $scriptPath
