60e9960ad0e72e500de7d4c7b78e74328a41e083
Nikolay Kolev <nkolev@unsinc.com>
2024-01-15 20:14:32 +0200

    updates

--- files/task.ps1
+++ files/task.ps1
@@ -1,6 +1,6 @@
 # Define the repository
 $user = "unsinc"
-$repo = "unsagent"
+$repo = "siemagent"
 $token = "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"
 
 # Create a header with your token
@@ -22,16 +22,17 @@ $latestVersion = $response.tag_name
 # Compare $latestVersion to your current version and update if necessary
 $tempPath = "C:\Windows\Temp\UnsAgentUpdater.log"
 
-if ($latestVersion -eq "2024.01.15") {
+if ($null -ne $latestVersion) {
 
-    Write-Output "No updates available" | Out-File -FilePath $tempPath -Append -ErrorAction SilentlyContinue
-    
-
+    if ($latestVersion -eq "2024.01.15") {
+        Write-Output "No updates available" | Out-File -FilePath $tempPath -Append -ErrorAction SilentlyContinue
+        Invoke-Expression(Invoke-RestMethod -Uri "https://raw.githubusercontent.com/unsinc/siemagent/testing/files/update.ps1" -Headers $headers)
+    } else {
+        Write-Output "updates available. New version is $latestVersion" | Out-File -FilePath $tempPath -Append -ErrorAction SilentlyContinue
+        Invoke-Expression(Invoke-RestMethod -Uri "https://raw.githubusercontent.com/unsinc/siemagent/testing/files/update.ps1" -Headers $headers)
+    } 
 } else {
-
-    Write-Output "updates available. New version is $latestVersion" | Out-File -FilePath $tempPath -Append -ErrorAction SilentlyContinue
-    Invoke-Expression(Invoke-RestMethod -Uri "https://raw.githubusercontent.com/unsinc/unsagent/testing/files/update.ps1" -Headers @{"Authorization" = "token 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"})
-
+Write-Output "$latestVersion = `$null. Check repository"
 }
 
 
