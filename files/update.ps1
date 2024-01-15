df82239d661f4a3a22c18c00bb9c2fc3c063ec13
Nikolay Kolev <nkolev@unsinc.com>
2024-01-15 15:10:01 +0200

    Scheduled task updater

--- files/update.ps1
+++ files/update.ps1
@@ -1,3 +1,37 @@
+# Define the repository and your personal access token
+$user = "unsinc"
+$repo = "unsagent"
+$token = "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"
+
+# Create a header with your token
+$headers = @{
+    "Authorization" = "token $token"
+
+}
+
+try {
+    # Call the GitHub API
+    $response = Invoke-RestMethod -Uri "https://api.github.com/repos/$user/$repo/releases/latest" -Headers $headers
+}
+catch {
+    $errorMessage = $_.Exception
+    Write-Output $errorMessage
+}
+
+
+# Check the tag_name property for the latest release version
+$latestVersion = $response.tag_name
+
+# Compare $latestVersion to your current version and update if necessary
 Add-Type -AssemblyName System.Windows.Forms
-[System.Windows.Forms.MessageBox]::Show('No updates Available', 'UNS Agent Updater', [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+if ($latestVersion -eq "2024.01.15") {
+
+    [System.Windows.Forms.MessageBox]::Show('No updates Available', 'UNS Agent Updater', [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+
+} else {
+
+    [System.Windows.Forms.MessageBox]::Show('Updates available', 'UNS Agent Updater', [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+
+}
+
 
