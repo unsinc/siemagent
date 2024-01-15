8c03f985c63e2af703664ac6367e41fc068b916b
Nikolay Kolev <nkolev@unsinc.com>
2024-01-15 15:05:43 +0200

    Scheduled task updater

--- files/update.ps1
+++ files/update.ps1
@@ -1,1 +1,3 @@
-#Test file for scheduled tasks.
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.MessageBox]::Show('No updates Available', 'UNS Agent Updater', [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+
